0ef358655cbb3c94d0018273f61e289a1d30a93f
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2017-08-15 20:53:27 +0200

    Configure target version and add Kotlin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val kotlin_version by extra { "1.1.4" }
+
 plugins {
     kotlin("jvm")
 }
 
+// It is possible to configure each task separately (prod/test)
+// but this way is simpler
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
 repositories {
     jcenter()
+}
+
+dependencies {
+    compile(kotlin("stdlib-jre8", kotlin_version))
 }
